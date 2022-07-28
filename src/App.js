b56bc2de14499f7ea93843e4bbef8d587d10fb8e
Norman Arredondo <arredondo_norman25@outlook.es>
2022-07-28 12:18:19 -0500

    Creating a new form
    Creating a new form to save information using localstorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //Ejecutar useState antes de que se renderice nuestro componente
+  //useState devuelve un arreglo, la prmera es una variable y la segunda es una funcion que permite llenar la variable
+  const [newTaskName, setNewTaskName] = useState();
+
+  const handleSubmit = (e) =>{
+    e.preventDefalt();
+    localStorage.setItem('tasks', newTaskName)
+    setNewTaskName('')
+  }
+
   return (
     <div className="App">
-     
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value= {newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+        />
+        <button>Save task</button>
+      </form>
     </div>
   );
 }
